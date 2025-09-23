668e632edf250f4bd4c3c3959443c51d64b414b7
enoseven7 <wratioethan@gmail.com>
2025-09-23 12:21:40 +0100

    Added scripts for Card and AtkDice Scriptable Objects

---
+++ TurnBasedDeckBuilder/Assets/Scripts/AtkDiceScriptableObject.cs
@@ -0,0 +1,56 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "AtkDiceScriptableObject", menuName = "ScriptableObjects/AtkDiceScriptableObject")]
+public class AtkDiceScriptableObject : ScriptableObject
+{
+    // Using an enumerator for different but specific types of attacks
+    public enum AttackType
+    {
+        Blunt,
+        Pierce,
+        Slash,
+        Block,
+        Evade,
+        Counter
+    }
+
+    // Using an enumerator for different but specific types of status effects
+    public enum StatusEffects
+    {
+        Burn,
+        Bleed,
+        Poise,
+        Charge,
+        Bind,
+        Haste,
+        Protection,
+        Strength,
+        Fragile
+    }
+
+    // Creating the variables for the attack dice, having a private version where the value is stored and a public version where the value can be accessed but not changed
+
+    [SerializeField]
+    float damage;
+    public float Damage { get => damage; private set => damage = value; }
+
+    [SerializeField]
+    AttackType atkType;
+    public AttackType AtkType  { get => atkType; private set => atkType = value; }
+
+    [SerializeField]
+    StatusEffects status;
+    public StatusEffects Status { get => status; private set => status = value; }
+
+    [SerializeField]
+    int statusCount;
+    public int StatusCount { get => statusCount; private set => statusCount = value; }
+
+    [SerializeField]
+    int minRoll;
+    public int MinRoll { get => minRoll; private set => minRoll = value; }
+
+    [SerializeField]
+    int maxRoll;
+    public int MaxRoll { get => maxRoll; private set => maxRoll = value; }
+}
---
+++ TurnBasedDeckBuilder/Assets/Scripts/CardScriptableObject.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(fileName = "CardScriptableObject", menuName = "ScriptableObjects/CardScriptableObject")]
+public class CardScriptableObject : ScriptableObject
+{
+    [SerializeField]
+    AtkDiceScriptableObject[] atkDice;
+    public AtkDiceScriptableObject[] AtkDice { get => atkDice; private set => atkDice = value; }
+
+    [SerializeField]
+    int energyCost;
+    public int EnergyCost { get => energyCost; private set => energyCost = value; }
+
+    [SerializeField]
+    Image cardImg;
+    public Image CardImg { get => cardImg; private set => cardImg = value; }
+
+    [SerializeField]
+    string cardName;
+    public string CardName { get => cardName; private set => cardName = value; }
+
+}
